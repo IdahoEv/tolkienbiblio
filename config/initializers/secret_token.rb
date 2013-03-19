bd77a7b3ee2e69f4e5f99977d47fd3cdab556fa1
Evan Dorn <evan@lrdesign.com>
2013-03-18 22:19:40 -0700

    Add initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tolkienbiblio::Application.config.secret_key_base = 'cd519f91328d05e795200e3b19d8400ca6e77877095c41731ff2a935edec1d2b0aa7a88e12d390598ebb03ad13a9429dc463496694c90a24eeb465cf591001bc'
